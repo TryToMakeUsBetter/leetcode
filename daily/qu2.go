f1a6f51a04b5b5b7e951ae3c15c3ae6b0fcf07fa
Gutianyu <gutianyu9871@gmail.com>
2025-01-16 20:54:38 +0800

    qu2

---
+++ daily/qu2.go
@@ -0,0 +1,26 @@
+package daily
+
+func RemoveElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	tail := len(nums) - 1
+	i := 0
+	for ; i < tail; i++ {
+		if nums[i] == val {
+			nums[tail], nums[i] = nums[i], nums[tail]
+			i--
+			tail--
+		}
+
+	}
+
+	if nums[i] == val {
+		nums[tail], nums[i] = nums[i], nums[tail]
+		i--
+		tail--
+	}
+
+	return i + 1
+}
