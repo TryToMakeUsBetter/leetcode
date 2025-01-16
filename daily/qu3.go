2526e1d09f3a46db1dd5a1936b761170335cf8d2
Gutianyu <gutianyu9871@gmail.com>
2025-01-16 21:08:03 +0800

    qu3

---
+++ daily/qu3.go
@@ -0,0 +1,18 @@
+package daily
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+
+	slow := 0
+
+	for fast := slow + 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[slow] {
+			nums[slow+1] = nums[fast]
+			slow++
+
+		}
+	}
+	return slow + 1
+}
