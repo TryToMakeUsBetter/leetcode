e64e555d024dc3f77529300eb1089b2a2b3f5a58
Gutianyu <gutianyu9871@gmail.com>
2025-01-17 19:43:00 +0800

    lc121

--- daily/qu7.go
+++ daily/qu7.go
@@ -2,16 +2,13 @@ package daily
 
 // lc121
 func maxProfit(prices []int) int {
-	dp := make([][]int, len(prices))
-	for i := 0; i < len(prices); i++ {
-		dp[i] = make([]int, 2)
-	}
+	dp := make([][2]int, len(prices))
 
-	dp[0][0] -= prices[0]
-	dp[0][1] = 0
+	dp[0][0] = 0
+	dp[0][1] = -prices[0]
 	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], -prices[i])
-		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
+		dp[i][0] = max(dp[i-1][1]+prices[i], dp[i-1][0])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
 	}
 
 	return dp[len(prices)-1][1]
