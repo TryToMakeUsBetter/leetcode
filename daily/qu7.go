bbded0c4352ec9fd91868a3f76b7ddcae56aa69f
Gutianyu <gutianyu9871@gmail.com>
2025-01-20 21:56:22 +0800

    lc121

--- daily/qu7.go
+++ daily/qu7.go
@@ -7,14 +7,14 @@ func maxProfit(prices []int) int {
 	dp[0][0] = -prices[0]
 	dp[0][1] = 0
 	for i := 1; i < len(prices); i++ {
-		dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
-		dp[i][0] = max(dp[i-1][0], -prices[i])
+		dp[i][1] = GetMax(dp[i-1][1], dp[i-1][0]+prices[i])
+		dp[i][0] = GetMax(dp[i-1][0], -prices[i])
 	}
 
 	return dp[len(prices)-1][1]
 }
 
-func max(a int, b int) int {
+func GetMax(a int, b int) int {
 	if a > b {
 		return a
 	}
